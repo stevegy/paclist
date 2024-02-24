881fda422ed7ceba0b6ce8c9929291ae824dd23f
Steve Yao <stevegy@gmail.com>
2024-02-24 17:58:11 +0800

    get the list use https proxy

--- routers/proxyPAC.js
+++ routers/proxyPAC.js
@@ -1,14 +1,75 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/proxy.pac', (req, res) => {
-  // if the GFW list on the disk file is available and the timestamp is not expired.
-  // get the GFW list from the disk file
-  // else get it from the configured URL and save it to disk file and set the expiry time
-  res.status(200);
-  res.setHeader('Content-Type', 'application/x-ns-proxy-autoconfig');
-  res.send();
+const axios = require('axios');
+const hpagent = require('hpagent');
+const config = require('../config');
+let pac = {
+  time: new Date().getTime(),
+  content: ''
+};
+
+router.get('/pac', async (req, res) => {
+  try {
+    // if the GFW list on the disk file is available and the timestamp is not expired.
+    // get the GFW list from the disk file
+    // else get it from the configured URL and save it to disk file and set the expiry time
+    if (pac.content === '' || 
+        pac.time < new Date().getTime() - config.pacExpiry * 60 * 1000) {
+      console.log(`${new Date().toISOString()} Fetching proxy list from ${config.listUrl}...`);
+      pac.time = new Date().getTime();
+      pac.content = await getPacContent();
+    }
+
+    res.status(200);
+    res.setHeader('Content-Type', 'application/x-ns-proxy-autoconfig');
+    // res.setHeader('Content-Type', 'text/plain');
+    res.send(pac.content);
+    console.log(`Proxy auto-config file generated at ${new Date().toISOString()}`);
+  } catch (e) {
+    console.error(e);
+    res.status(e.response?.status || 500).send(
+      e.response?.statusText || 'Internal Server Error');
+  }
 });
 
+async function getPacContent() {
+  const rawData = Buffer.from(await getProxyList(), 'base64').toString('utf-8');
+  const rawList = rawData.split('\n');
+  const pacList = [].concat(config.predefined);
+  for (const item of rawList) {
+    if (item.startsWith('.')) {
+      pacList.push(item.slice(1));
+    }
+    if (item.startsWith('||')) {
+      pacList.push(item.slice(2));
+    }
+  }
+  const pacArray = Array.from(new Set(pacList));
+  const content = `var proxy_yes = "PROXY ${config.proxy.host}:${config.proxy.port}";
+var domains = [${pacArray.map(i => `"${i}"`).join(',')}];
+function FindProxyForURL(url, host) {
+  if (shExpMatch(url, "${config.exclude}")) {
+    return "DIRECT";
+  }
+  for (var i = 0; i < domains.length; i++) {
+    if (dnsDomainIs(host, domains[i])) {
+      return proxy_yes;
+    }
+  }
+  return "DIRECT";
+}`;
+  return content;
+}
+
+async function getProxyList() {
+  // set the proxy for axios
+  const httpsProxyAgent = new hpagent.HttpsProxyAgent({
+    proxy: `http://${config.proxy.host}:${config.proxy.port}`
+  });
+  const response = await axios.get(config.listUrl,
+    { httpsAgent: httpsProxyAgent });
+  return response.data;
+}
 
 module.exports = router;
